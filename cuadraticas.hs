1c068dc149b06f5987180c07498559bc48798ada
Alan-Vazquez <vazalan17@ciencias.unam.mx>
2024-08-24 10:43:40 -0600

    Add files via upload

--- cuadraticas.hs
+++ cuadraticas.hs
@@ -1,12 +1,12 @@
 --Raices de una ecuación cuadratica--
 
-tupla (Float, Float)
-formula :: Float->Float->Float->tupla
+type Tupla = (Float, Float)
+
+formula :: Float->Float->Float->Tupla
 formula a b c =
-let discriminante = (b*b)-4*a*c
-in
-if discriminante >= 0
-then
-let x1 = (-b-(sqrt((discriminante))))/(2*a)  
-let x2 = -b+(sqrt((discriminante)))/(2*a)
+     let discriminante = (b*b)-4*a*c
+     in if discriminante >= 0
+then let x1 = (-b-(sqrt((discriminante))))/(2*a)  
+         x2 = -b+(sqrt((discriminante)))/(2*a)
+     in (x1, x2)
 else error "La ecuacuion no tiene raices reales"
